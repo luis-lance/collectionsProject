e7a4e176ffe8df5968b5e4d1262e86cc538fc77a
Luis Delgado <delgadoluis0510@gmail.com>
2019-09-30 16:04:23 -0500

    updates

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -32,31 +32,42 @@ public class GradesApplication {
         bob.addGrade(66);
         bob.addGrade(55);
 
-        students.put("lance", " @glitch78");
-        students.put("luis", " @luisdelgado373");
-        students.put("alex", " @alex098");
-        students.put("bob", " @bobs123");
+        students.put("lance", "glitch78");
+        students.put("luis", "luis210");
+        students.put("alex", "alex098");
+        students.put("bob", "bobs123");
 
 
-        System.out.printf("Welcome!\nHere are the github usernames of our students:\n| %s |\t| %s |\t| %s |\t| %s |\n\n", students.get("lance"), students.get("luis"), students.get("alex"), students.get("bob"));
-        System.out.println("What student would you like to see more information on?");
+        System.out.printf("Welcome!\nHere are the github usernames of our students:\n| %s |   | %s |   | %s |   | %s |\n\n", students.get("lance"), students.get("luis"), students.get("alex"), students.get("bob"));
         do {
+            System.out.println("What student would you like to see more information on?");
             String response = input.nextLine();
-            if (response.equalsIgnoreCase("glitch78")) {
-                System.out.printf("Name: Lance - GitHub Username: @glitch78\nCurrent Average: %f\n\nWould you like to see another student?", lance.getGradeAverage());
-            } else if (response.equalsIgnoreCase("luis210")) {
-                System.out.printf("Name: Luis - GitHub Username: @luis210\nCurrent Average: %f\n\nWould you like to see another student?", luis.getGradeAverage());
-            } else if (response.equalsIgnoreCase("alex098")) {
-                System.out.printf("Name: Alex - GitHub Username: @alex098\nCurrent Average: %f\n\nWould you like to see another student?", alex.getGradeAverage());
-            } else if (response.equalsIgnoreCase("bobs123")) {
-                System.out.printf("Name: Bob - GitHub Username: @bobs123\nCurrent Average: %f\n\nWould you like to see another student?", bob.getGradeAverage());
-            } else if (response.equalsIgnoreCase("no")) {
-
-            } else {
-                System.out.printf("Sorry, no student found with the GitHub username of \"%s\"\nWould you like to see another student?\n", response);
+            for (Map.Students<String, String> e : hashmap.entrySet()) {
+                String key = e.getKey();
+                String value = e.getValue();
             }
-        } while(willContinue("Would you like to see another student?"));
+            if (students.containsValue(response)) {
+                System.out.printf("Name: %s - GitHub Username: %s\nCurrent Average: %f\n\n", students.get("lance"), students.values(), lance.getGradeAverage());
+            } else if (response.equalsIgnoreCase("no")) {
+                System.out.println("Goodbye, and have a wonderful day!");
+            } else {
+                System.out.printf("Sorry, no student found with the GitHub username of \"%s\"\n\n", response);
+            }
+        } while (willContinue("Would you like to see another student?"));
+        System.out.println("Goodbye, and have a wonderful day!");
+    }
 
+    public static boolean willContinue(String prompt) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(prompt);
+        String response = input.nextLine();
+        if (response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")) {
+            return true;
+        } else if (response.equalsIgnoreCase("no") || response.equalsIgnoreCase("n")) {
+
+            return false;
+        }
+        return false;
     }
 
 }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,5 @@
 package grades;
+
 import java.awt.*;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -44,16 +45,4 @@ public class Student {
 //        System.out.println(luis);
     }
 
-    public boolean willContinue(String prompt){
-        Scanner input = new Scanner(System.in);
-        String response = input.nextLine();
-        if(response.equalsIgnoreCase("yes")|| response.equalsIgnoreCase("y")){
-            System.out.println("What student would you like to see information on");
-
-        } else if (response.equalsIgnoreCase("no")|| response.equalsIgnoreCase("n")){
-            System.out.println("Goodbye, have a wonderful day!");
-
-        }
-        return false;
-    }
 }
